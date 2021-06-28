31cb9fe65b4f6923e034dcc06825cedb95ec3574
daeungkim <daeungkim93@gmail.com>
2021-06-28 09:25:47 +0900

    update:merge-descriptor 분석 완료
    <description>
    merge-descriptor 분석 완료
    <issue-number>

--- index.js
+++ index.js
@@ -22,6 +22,10 @@ module.exports = merge
 var hasOwnProperty = Object.prototype.hasOwnProperty
 
 /**
+ * hasOwnProperty : 개체가 특정 프로퍼티를 소유하고 있는지 판단하는데 사용한다. / 개체의 프로토타입 체인을 확인하지 않는다.
+ */
+
+/**
  * Merge the property descriptors of `src` into `dest`
  *
  * @param {object} dest Object to add descriptors to
@@ -57,6 +61,9 @@ function merge (dest, src, redefine) {
 
   Object.getOwnPropertyNames(src).forEach(function forEachOwnPropertyName (name) {
     if (!redefine && hasOwnProperty.call(dest, name)) {
+      /**
+       * Function.prototype.call : 첫번째 매개변수를 this, 나머지 매개변수를 인수로 하여 함수를 호출한다.
+       */
       // Skip descriptor
       return
     }
@@ -67,7 +74,13 @@ function merge (dest, src, redefine) {
 
     // Copy descriptor
     var descriptor = Object.getOwnPropertyDescriptor(src, name)
+    /**
+     * Object.getOwnPropertyDescriptor : 주어진 개체의 프로퍼티에 대한 속성 설명자(descriptor)를 반환한다.
+     */
     Object.defineProperty(dest, name, descriptor)
+    /**
+     * Object.defineProperty : 개체에 직접 새로운 속성을 정의하거나 이미 존재하는 속성을 수정한 후, 그 개체를 반환한다.
+     */
     /**
      * src의 프로퍼티를 dest에 복사한다.
      */
